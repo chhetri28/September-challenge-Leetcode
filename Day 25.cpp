f6619c30836bcab39fe47ae3d50e48fed1717289
chhetri28 <chettrisourav28@gmail.com>
2020-09-26 18:55:09 +0530

    Day 25

---
+++ Day 25.cpp
@@ -0,0 +1,30 @@
+// Largest Number
+/*
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+Example 1:
+
+Input: [10,2]
+Output: "210"
+
+Example 2:
+
+Input: [3,30,34,5,9]
+Output: "9534330"
+
+*/
+class Solution {
+public:
+    string largestNumber(vector<int>& num) {
+       vector<string> arr;
+        for(auto i:num)
+            arr.push_back(to_string(i));
+        sort(begin(arr), end(arr), [](string &s1, string &s2){ return s1+s2>s2+s1; });
+        string res;
+        for(auto s:arr)
+            res+=s;
+        while(res[0]=='0' && res.length()>1)
+            res.erase(0,1);
+        return  res;
+    }
+};
